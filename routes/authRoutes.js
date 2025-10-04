3c07b5835e808139b613735ef68e20e396740806
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-10-04 20:47:59 +0800

    Add auth routes and controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const { register } = require('../controllers/authController');
 
+// Register a user
 router.post('/register', register);
+
+// Login a user
+router.post('/login', login);
 
 module.exports = router;
