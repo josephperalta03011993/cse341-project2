0ec4c0346b41cdfe3b1b88a6b9378c34e29d91af
Peralta <joseph.peralta03011993@gmail.com>
2025-10-04 11:59:00 +0800

    add middleware auth

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
+
+  if (!token) return res.status(401).json({ error: "No token, access denied" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ error: "Invalid token" });
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = authMiddleware;
